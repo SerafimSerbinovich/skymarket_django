f9491aa4ecc19f8a9b9fb8dedf09cd65a6c75b40
Serafim <serafimabove@mail.ru>
2023-02-24 19:05:27 +0300

    init

--- skymarket/ads/models.py
+++ skymarket/ads/models.py
@@ -1,12 +1,20 @@
 from django.conf import settings
 from django.db import models
+from skymarket.settings import MEDIA_ROOT
+from users.models import CustomUser
 
 
 class Ad(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    title = models.CharField(max_length=256)
+    price = models.SmallIntegerField()
+    description = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to=MEDIA_ROOT, blank=True, null=True)
 
 
 class Comment(models.Model):
-    # TODO добавьте поля модели здесь
-    pass
+    text = models.TextField()
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
